dd5be9ec37d876a7cf41068588d6707e6ded0b5b
RITIK SHARMA <ritiksharma555598@gmail.com>
2024-11-30 21:43:22 +0530

    changing to project title

--- front_app_resume/src/container/Work/Work.jsx
+++ front_app_resume/src/container/Work/Work.jsx
@@ -41,7 +41,7 @@ const Work = () => {
       <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
 
       <div className="app__work-filter">
-        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+        {['RESPONSIVE', 'JAVASCRIPT', 'REACT JS', 'MERN', 'JSP/SERVLET', 'SPRING/SPRING-BOOT', 'ALL'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
