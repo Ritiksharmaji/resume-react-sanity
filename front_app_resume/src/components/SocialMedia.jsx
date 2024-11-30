fd6d922de61a137d750e6cecd129a05ac1f11fd2
Ritiksharmaji <ritiksharma555598@gmail.com>
2024-11-30 16:15:14 +0530

    change the data

--- front_app_resume/src/components/SocialMedia.jsx
+++ front_app_resume/src/components/SocialMedia.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsTwitter, BsInstagram, BsLinkedin } from 'react-icons/bs';
+import { FaFacebookF, FaCode } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <a href="https://instagram.com/ritikro3211" target="_blank" rel="noopener noreferrer">
+        <BsInstagram  />
+      </a>
     </div>
     <div>
-      <FaFacebookF />
+      <a href="https://facebook.com/profile.php?id=100028110709919" target="_blank" rel="noopener noreferrer">
+        <FaFacebookF />
+      </a>
     </div>
     <div>
-      <BsInstagram />
+      <a href="https://twitter.com/Ritiksh98087502" target="_blank" rel="noopener noreferrer">
+        <BsTwitter />
+      </a>
     </div>
+    <div>
+      <a href="https://linkedin.com/in/ritiksharmaji" target="_blank" rel="noopener noreferrer">
+        <BsLinkedin />
+      </a>
+    </div>
+    <div>
+      <a href="https://www.codechef.com/users/ritiksharma55" target="_blank" rel="noopener noreferrer">
+        <FaCode />
+      </a>
+    </div>
+    
   </div>
 );
 
--- front_app_resume/src/container/Header/Header.jsx
+++ front_app_resume/src/container/Header/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => (
           <span>👋</span>
           <div style={{ marginLeft: 20 }}>
             <p className="p-text">Hello, I am</p>
-            <h1 className="head-text">Ritik</h1>
+            <h1 className="head-text">RITIK SHARMA</h1>
           </div>
         </div>
 
